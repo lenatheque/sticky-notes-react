f7a6171982ade9888918bfe0cc7733042e292d1b
✬ ۞ ☾ Lorεηα ☽ ۞ ✬ <45369211+lenatheque@users.noreply.github.com>
2021-04-28 10:01:53 -0600

    defining onSearch method

--- src/App.js
+++ src/App.js
@@ -8,16 +8,10 @@ presentational components. */
 
 class App extends Component {
   state = {
-    notes: [
-      {
-        id: Date.now,
-        title: "",
-        description: "",
-        doesMatchSearch: true
-      }
-    ],
+    notes: [],
     searchText: ""
   };
+
   addNote = () => {
     const newNote = {
       id: Date.now(),
@@ -28,6 +22,32 @@ class App extends Component {
     const newNotes = [newNote, ...this.state.notes];
     this.setState({ notes: newNotes });
   };
+  onType = (editMeId, updatedKey, updatedValue) => {
+    /* this event handler updates sticky note text fields
+      - editMeId: the id of the note that the user typed in
+      - updatedKey: which field was edited? 'title' or 'description'
+      - updatedValue: new value of edited field */
+    const updateIdMatch = (note) => {
+      if (note.id !== editMeId) {
+        return note;
+      } else {
+        if (updatedKey === "title") {
+          return {
+            ...note,
+            title: updatedValue
+          };
+        } else {
+          return {
+            ...note,
+            description: updatedValue
+          };
+        }
+      }
+    };
+    const updatedNotes = this.state.notes.map(updateIdMatch);
+    this.setState({ notes: updatedNotes });
+  };
+
   render() {
     return (
       <div>
